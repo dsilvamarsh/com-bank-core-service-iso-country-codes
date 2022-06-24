391f5b10e33888e529f9ea6a305371d06834f360
Marsh Dsilva <dsilvamarsh@gmail.com>
2022-06-24 22:28:05 +0100

    initial commit

---
+++ src/main/java/com/bank/core/service/config/ComBankCoreServiceIsoCountryCodesApplication.java
@@ -0,0 +1,13 @@
+package com.bank.core.service.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComBankCoreServiceIsoCountryCodesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComBankCoreServiceIsoCountryCodesApplication.class, args);
+	}
+
+}
