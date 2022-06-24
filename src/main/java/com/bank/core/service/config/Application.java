a175e5d02ccb47562428e7ec4bc8f0cfa92bb08d
Marsh Dsilva <dsilvamarsh@gmail.com>
2022-06-24 22:42:02 +0100

    initial commit with country codes

--- src/main/java/com/bank/core/service/config/Application.java
+++ src/main/java/com/bank/core/service/config/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ComBankCoreServiceIsoCountryCodesApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ComBankCoreServiceIsoCountryCodesApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
